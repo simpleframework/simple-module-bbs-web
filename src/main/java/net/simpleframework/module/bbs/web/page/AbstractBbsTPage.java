b90e1fc07c6e4367d975ed7485b74ad82a5cd5c0
simpleframework <cknet@126.com>
2013-11-15 12:11:47 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/bbs/web/page/AbstractBbsTPage.java
+++ src/main/java/net/simpleframework/module/bbs/web/page/AbstractBbsTPage.java
@@ -36,8 +36,7 @@ import net.simpleframework.mvc.template.lets.TopBar_PageletsPage;
 /**
  * Licensed under the Apache License, Version 2.0
  * 
- * @author 陈侃(cknet@126.com, 13910090885)
- *         https://github.com/simpleframework
+ * @author 陈侃(cknet@126.com, 13910090885) https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public abstract class AbstractBbsTPage extends TopBar_PageletsPage implements IBbsContextAware {
